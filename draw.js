e939795c5530c81b28bb863c8c35febd451f44b7
Nikolay Martyanov <ohmspectator@gmail.com>
2023-01-15 16:18:07 +0100

    Hide the node edges if the node is deselected.
    Leave only one of them.

--- draw.js
+++ draw.js
@@ -1,36 +1,68 @@
 function draw() {
-    var graph = vis.parseDOTNetwork(dotStr);
-    var options = {
-        manipulation: {
-            enabled: true
-        }
-    };
-    var container = document.getElementById('mynetwork');
-    var network = new vis.Network(container, graph, options);
+    // parse dot file to extract nodes and edges
+    var dot = vis.parseDOTNetwork(dotStr);
+    var nodes = dot.nodes;
+    var edges = dot.edges;
 
-    var allEdges = graph.edges;
-    var selectedNode;
-    network.on("selectNode", function (params) {
-        if (params.nodes.length === 1) {
-            selectedNode = params.nodes[0];
-            allEdges.forEach(function (edge) {
-                if (edge.from !== selectedNode && edge.to !== selectedNode) {
-                    network.updateEdge(edge.id, {hidden: true});
-                } else {
-                    network.updateEdge(edge.id, {hidden: false});
-                }
-            });
+    // create a map to store unique edges
+    var uniqueEdges = new Map();
+
+    // iterate through edges and add unique edges to the map
+    edges.forEach(function (edge) {
+        var from = edge.from;
+        var to = edge.to;
+        var edgeId = from + "-" + to;
+        var reverseEdgeId = to + "-" + from;
+
+        // check if the edge or its reverse already exists in the map
+        if (!uniqueEdges.has(edgeId) && !uniqueEdges.has(reverseEdgeId)) {
+            uniqueEdges.set(edgeId, edge);
+        } else {
+            edge.hidden = true;
         }
     });
 
-    network.on("deselectNode", function (params) {
-        if (params.previousSelection.nodes.length === 1) {
-            var previousSelectedNode = params.previousSelection.nodes[0];
-            allEdges.forEach(function (edge) {
-                if (edge.from === previousSelectedNode || edge.to === previousSelectedNode) {
-                    network.updateEdge(edge.id, {hidden: false, label: ""});
-                }
+    // convert map to an array of edges
+
+    // create a map to store original edges for each node
+    var nodeEdges = new Map();
+    nodes.forEach(function (node) {
+        var nodeId = node.id;
+        var nodeEdgesArray = edges.filter(function (edge) {
+            return edge.from === nodeId || edge.to === nodeId;
+        });
+        nodeEdges.set(nodeId, nodeEdgesArray);
+    });
+
+    // create a network visualization
+    var container = document.getElementById("network");
+    var data = {
+        nodes: nodes,
+        edges: edges
+    };
+    var options = {};
+    var network = new vis.Network(container, data, options);
+
+    // add event listener to handle node selection
+    network.on("selectNode", function (params) {
+        if (params.nodes.length > 0) {
+            var selectedNodeId = params.nodes[0];
+            var selectedNodeEdges = nodeEdges.get(selectedNodeId);
+            selectedNodeEdges.forEach(function (edge) {
+                network.updateEdge(edge.id, {hidden: false});
+            });
+        } else {
+            edges.forEach(function (edge) {
+                network.updateEdge(edge.id, {hidden: true});
             });
         }
+    });
+    network.on("deselectNode", function (params) {
+        edges.forEach(function (edge) {
+            edgeId = edge.from + "-" + edge.to;
+            if (!uniqueEdges.has(edgeId)) {
+                network.updateEdge(edge.id, {hidden: true});
+            }
+        });
     });
 }
