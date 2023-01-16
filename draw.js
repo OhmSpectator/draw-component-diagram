df21840f3093f3e5eb00cd1bcf90fc13e272a2f9
Nikolay Martyanov <ohmspectator@gmail.com>
2023-01-16 12:51:41 +0100

    Improve visualization.

--- draw.js
+++ draw.js
@@ -1,53 +1,75 @@
+
+
 function draw() {
     // parse dot file to extract nodes and edges
-    var dot = vis.parseDOTNetwork(dotStr);
-    var nodes = dot.nodes;
-    var edges = dot.edges;
+    let dot = vis.parseDOTNetwork(dotStr);
+    let nodes = dot.nodes;
+    let edges = dot.edges;
 
     // create a map to store unique edges
-    var uniqueEdges = new Map();
+    let uniqueEdges = new Map();
+
+    function getUniqueEdgeId(from, to) {
+        return from + "-" + to;
+    }
 
     // iterate through edges and add unique edges to the map
     edges.forEach(function (edge) {
-        var from = edge.from;
-        var to = edge.to;
-        var edgeId = from + "-" + to;
-        var reverseEdgeId = to + "-" + from;
+        let edgeId = getUniqueEdgeId(edge.from, edge.to);
+        let reverseEdgeId = getUniqueEdgeId(edge.to, edge.from);
 
-        // check if the edge or its reverse already exists in the map
-        if (!uniqueEdges.has(edgeId) && !uniqueEdges.has(reverseEdgeId)) {
+        let hide = true;
+        if (!uniqueEdges.has(edgeId)) {
             uniqueEdges.set(edgeId, edge);
-        } else {
+            hide = false;
+        }
+        if (!uniqueEdges.has(reverseEdgeId)) {
+            uniqueEdges.set(reverseEdgeId, edge);
+            hide = false;
+        }
+        if (hide) {
             edge.hidden = true;
         }
     });
 
-    // convert map to an array of edges
-
     // create a map to store original edges for each node
-    var nodeEdges = new Map();
+    let nodeEdges = new Map();
     nodes.forEach(function (node) {
-        var nodeId = node.id;
-        var nodeEdgesArray = edges.filter(function (edge) {
+        let nodeId = node.id;
+        let nodeEdgesArray = edges.filter(function (edge) {
             return edge.from === nodeId || edge.to === nodeId;
         });
         nodeEdges.set(nodeId, nodeEdgesArray);
     });
 
     // create a network visualization
-    var container = document.getElementById("network");
-    var data = {
+    let container = document.getElementById("network");
+    let data = {
         nodes: nodes,
         edges: edges
     };
-    var options = {};
-    var network = new vis.Network(container, data, options);
+    let options = {
+        physics: {
+            barnesHut: {
+                gravitationalConstant: -8000,
+                centralGravity: 0.3,
+                springLength: 95,
+                springConstant: 0.04,
+                avoidOverlap: 0
+            }
+        },
+        layout: {
+            improvedLayout: true
+        },
+    };
+
+    let network = new vis.Network(container, data, options);
 
     // add event listener to handle node selection
     network.on("selectNode", function (params) {
         if (params.nodes.length > 0) {
-            var selectedNodeId = params.nodes[0];
-            var selectedNodeEdges = nodeEdges.get(selectedNodeId);
+            let selectedNodeId = params.nodes[0];
+            let selectedNodeEdges = nodeEdges.get(selectedNodeId);
             selectedNodeEdges.forEach(function (edge) {
                 network.updateEdge(edge.id, {hidden: false});
             });
@@ -59,8 +81,9 @@ function draw() {
     });
     network.on("deselectNode", function (params) {
         edges.forEach(function (edge) {
-            edgeId = edge.from + "-" + edge.to;
-            if (!uniqueEdges.has(edgeId)) {
+            let edgeId = getUniqueEdgeId(edge.from, edge.to);
+            let reverseEdgeId = getUniqueEdgeId(edge.to, edge.from);
+            if (uniqueEdges.get(edgeId) !== edge && uniqueEdges.get(reverseEdgeId) !== edge) {
                 network.updateEdge(edge.id, {hidden: true});
             }
         });
